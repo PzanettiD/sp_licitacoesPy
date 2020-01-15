793d8b1f944d98cddc096b28798e84d108ca1708
PzanettiD <zanetti.pepe@gmail.com>
2020-01-15 02:05:41 -0300

    commit inicial do pacote

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name='sp_licitacoesPy',
+        version='0.1',
+        description='Python3 Wrapper para API de licitações da Prefeitura de São Paulo',
+        py_modules=["sp_licitacoesPy"],
+        package_dir={'': 'src'},
+)
