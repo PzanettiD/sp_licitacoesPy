c4bfab1beb4854fa0d8d308d82c46e5454acf12d
PzanettiD <zanetti.pepe@gmail.com>
2020-01-18 02:15:53 -0300

    versão 0.0.2

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
         long_description = fh.read()
 
 setuptools.setup(
-        name="sp_licitacoesPy", # Replace with your own username
-        version="0.0.1",
+        name="sp_licitacoesPy", 
+        version="0.0.2",
         author="PzanettiD",
         author_email="zanetti.pepe@gmail.com",
-        description="Python3 Wrapper para API de licitações da Prefeitura de São Paulo",
+        description="Simples wrapper para API de licitações da Prefeitura de São Paulo",
         long_description=long_description,
         long_description_content_type="text/markdown",
         url="https://github.com/PzanettiD/sp_licitacoesPy",
